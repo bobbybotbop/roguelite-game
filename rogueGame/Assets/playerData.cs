f7394d1728b1f2b5ba82886d87109868c09ec0d2
bobbybotbop <bobbybotbop@gmail.com>
2022-05-22 20:12:51 -0400

    a

--- rogueGame/Assets/playerData.cs
+++ rogueGame/Assets/playerData.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class playerData : MonoBehaviour
 {
-
     public HealthBar bar;
-
     public int health = 100;
+    public float attackSpeed = 1.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
         bar.SetMaxHealth(health);
+        InvokeRepeating("test", 0, attackSpeed);
     }
 
     // Update is called once per frame
@@ -26,6 +26,10 @@ public class playerData : MonoBehaviour
     {
         health -= n;
         bar.SetHealth(health);
+    }
+    public void test()
+    {
+        Debug.Log("gay");
     }
 
 }
