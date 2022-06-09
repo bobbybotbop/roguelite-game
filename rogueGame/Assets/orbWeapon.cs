d2293c8bbbff8d162fd7a4c54e113da1cecb7262
William Chen <bobbybotbop@gmail.com>
2022-06-09 18:40:18 -0400

    added orb

--- rogueGame/Assets/orbWeapon.cs
+++ rogueGame/Assets/orbWeapon.cs
@@ -7,12 +7,15 @@ public class orbWeapon : weaponClass
     // Start is called before the first frame update
     public Transform rotationCenter;
 
-    float rotationRadius = 2f;
+    float rotationRadius = 2.5f;
     float posX, posY, angle = 0f;
     float rotationSpeed;
+
+    bool isActive = true;
+
     void Start()
     {
- 
+
     }
 
     // Update is called once per frame
@@ -22,17 +25,25 @@ public class orbWeapon : weaponClass
     }
     public new void Attack()
     {
-        rotationSpeed = attackSpeed;
-        posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
-        posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+        rotationSpeed = attackSpeed * level / 2;
+        posX = rotationCenter.position.x + Mathf.Sin(angle) * rotationRadius;
+        posY = rotationCenter.position.y + Mathf.Cos(angle) * rotationRadius;
         transform.position = new Vector2(posX, posY);
         angle = angle + Time.deltaTime * rotationSpeed;
 
-        if(angle >= 360f)
+        if (angle >= 360f)
         {
             angle = 0f;
+            wait();
         }
         coll.enabled = true;
 
+    }
+
+    IEnumerator wait()
+
+    {
+        yield return new WaitForSeconds(coolDown);
+        Debug.Log("");
     }
 }
